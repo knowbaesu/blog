2549d9893ae64882477bab4b8882b96f641bd5ba
pjs <vkrwltn12@gmail.com>
2022-08-16 03:07:43 +0900

    blog project

---
+++ src/topbar/Topbar.jsx
@@ -0,0 +1,11 @@
+import "./topbar.css"
+
+export default function Topbar() {
+  return (
+    <div className='top'>
+        <div className="topLeft">l</div>
+        <div className="topCenter">c</div>
+        <div className="topRight">r</div>
+    </div>
+  )
+}
