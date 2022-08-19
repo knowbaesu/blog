aa76f2a09787d2a2403167ed44bcee1e0dd52181
pjs <vkrwltn12@gmail.com>
2022-08-19 11:02:58 +0900

    post

--- src/Sidebar/Sidebar.jsx
+++ src/Sidebar/Sidebar.jsx
@@ -1,9 +1,41 @@
 import "./sidebar.css"
+import TwitterIcon from '@mui/icons-material/Twitter'
+import Instagram from "@mui/icons-material/Instagram"
+import FacebookIcon from '@mui/icons-material/Facebook';
+import PinterestIcon from '@mui/icons-material/Pinterest';
+
 
 export default function Sidebar() {
   return (
     <div className="sidebar">
-        sidebar
+        <div className="sidebarItem">
+          <span className="sidebarTitle">ABOUT ME</span>
+          <img src="https://e00-marca.uecdn.es/assets/multimedia/imagenes/2022/07/31/16592809381406.jpg" alt="" />
+          <p>
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dolores, 
+            unde cumque provident autem corporis esse repellat hic voluptatum?
+          </p>
+        </div>
+        <div className="sidebarItem">
+          <span className="sidebarTitle">CATEGORIES</span>
+          <ul className="sidebarList">
+            <li className="sidebarListItem">Life</li>
+            <li className="sidebarListItem">Music</li>
+            <li className="sidebarListItem">Style</li>
+            <li className="sidebarListItem">Sport</li>
+            <li className="sidebarListItem">Tech</li>
+            <li className="sidebarListItem">Cinema</li>
+          </ul>
+        </div>
+        <div className="sidebarItem">
+          <span className="sidebarTitle">CATEGORIES</span>
+          <div className="sidebarSocial">
+            <TwitterIcon className="sidebarIcon"/>
+            <Instagram className="sidebarIcon"/>
+            <FacebookIcon className="sidebarIcon"/>
+            <PinterestIcon className="sidebarIcon"/>
+          </div>  
+        </div>
     </div>
   )
 }
---
+++ src/post/Post.jsx
@@ -0,0 +1,24 @@
+import "./post.css"
+
+export default function Post() {
+  return (
+    <div className="post">
+        <img className="postImg" src="https://cdn.sportivomedia.net/news/photo/202204/2928_2811_288.jpg" alt="" />
+        <div className="postInfo">
+            <div className="postCats">
+                <span className="postCat">Music</span>
+                <span className="postCat">Life</span>
+            </div>
+            <span className="postTitle">Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+            </span>
+            <hr/>
+            <span className="postDate">1 hour ago</span>
+        </div>
+        <p className="postDesc">
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. 
+          Perferendis aut nihil doloremque placeat quidem architecto et, 
+          accusantium eaque dolorum maiores excepturi sunt commodi 
+        </p>
+    </div>
+  )
+}
