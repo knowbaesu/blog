8e57b579fda3ad165126a084e2512f9b5eb0bb81
pjs <vkrwltn12@gmail.com>
2022-08-25 23:03:07 +0900

    login front

--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -2,6 +2,16 @@ import "./login.css"
 
 export default function Login() {
   return (
-    <div>Login</div>
+    <div className="login">
+        <span className="loginTitle">Login</span>
+      <form className="longinForm">
+        <label>Email</label>
+        <input className="loginInput" type="text" />
+        <label>Password</label>
+        <input className="loginInput" type="password" />
+        <button className="loginButton">Login</button>
+      </form>
+        <button className="registerButton">Register</button>
+    </div>
   )
 }
