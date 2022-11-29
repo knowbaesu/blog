c93950869a0dafac61b643ba2cb4805a7e8e97fa
pjs <vkrwltn12@gmail.com>
2022-11-29 23:55:48 +0900

    reducer

---
+++ front/src/context/Reducer.js
@@ -0,0 +1,25 @@
+const Reducer = (state, action) => {
+    switch (action.type) {
+        case "Login_start":
+            return {
+                user:null,
+                insFetching:true,
+                error:false
+            };
+            case "Login_start":
+            return {
+                user:null,
+                insFetching:true,
+                error:false
+            
+        }
+        case "Login_start":
+            return {
+                user:null,
+                insFetching:true,
+                error:false
+            }
+            default:
+                return  state;
+    }
+}
